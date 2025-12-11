031290bf7365f019d81b2610274e07f472bdae86
Hugo Chung <chunghugo99994@gmail.com>
2025-12-11 08:38:38 -0800

    Feat: Remove user attribute similarity validator

--- student_forum/settings.py
+++ student_forum/settings.py
@@ -159,9 +159,6 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
