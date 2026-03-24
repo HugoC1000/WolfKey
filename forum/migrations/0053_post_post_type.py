b9e235bb6a8d0355c1104d17fa6116029b7fa230
Hugo Chung <chunghugo99994@gmail.com>
2026-03-24 22:41:08 +0800

    Fix: fix db changes

--- forum/migrations/0053_post_post_type.py
+++ forum/migrations/0053_post_post_type.py
@@ -10,13 +10,42 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='post',
-            name='post_type',
-            field=models.CharField(
-                choices=[('standard', 'Standard Post'), ('poll', 'Poll')],
-                default='standard',
-                max_length=20
-            ),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql='''
+                        ALTER TABLE forum_post ADD COLUMN IF NOT EXISTS post_type varchar(20);
+                        ALTER TABLE forum_post ALTER COLUMN post_type SET DEFAULT 'standard';
+
+                        UPDATE forum_post p
+                        SET post_type = 'poll'
+                        WHERE EXISTS (
+                            SELECT 1 FROM forum_poll fp WHERE fp.post_ptr_id = p.id
+                        );
+
+                        UPDATE forum_post p
+                        SET post_type = 'standard'
+                        WHERE NOT EXISTS (
+                            SELECT 1 FROM forum_poll fp WHERE fp.post_ptr_id = p.id
+                        );
+
+                        ALTER TABLE forum_post ALTER COLUMN post_type SET NOT NULL;
+                    ''',
+                    reverse_sql='''
+                        ALTER TABLE forum_post DROP COLUMN IF EXISTS post_type;
+                    '''
+                ),
+            ],
+            state_operations=[
+                migrations.AddField(
+                    model_name='post',
+                    name='post_type',
+                    field=models.CharField(
+                        choices=[('standard', 'Standard Post'), ('poll', 'Poll')],
+                        default='standard',
+                        max_length=20
+                    ),
+                ),
+            ]
         ),
     ]
